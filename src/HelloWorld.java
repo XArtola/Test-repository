8fb08e6fb7954e02845a5cff840772773ddb9c22
XArtola <xartolaza18dw@ikzubirimanteo.com>
2018-09-22 21:12:05 +0200

    e

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Bye bye, World");
+        System.out.println("How are you, World");
     }
 
 }
